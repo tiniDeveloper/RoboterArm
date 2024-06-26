ba7a9769932909c4fbfb90c26e851a2a9f0bdb7b
Aimce <122790901+Aimce@users.noreply.github.com>
2024-06-26 12:42:22 +0200

    Test Exceptions

--- test/java/de/developup/roboterarm/gui/GUIControllerTest.java
+++ test/java/de/developup/roboterarm/gui/GUIControllerTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Klasse zum Testen von wichtigen Methoden.
+ */
 class GUIControllerTest {
 
     @Test
@@ -33,5 +36,25 @@ class GUIControllerTest {
         assertThrows(GUIController.InvalidParameterException.class, () -> {
             GuiCheck.checkInputs(5,5,91);
         });
+    }
+
+    @Test
+    void bytesToInt(){
+        GUIController GuiCheck = new GUIController();
+
+
+        byte[] test = new byte[] {0,(byte)0x2C,(byte)0x1,(byte)0xD4,(byte)0xFE,(byte)0xF4,(byte)0x1,(byte)0xEC,(byte)0xFF};
+
+        int[] result = new int[] {300,-300,500,-20};
+
+        int[] actual = GuiCheck.bytesToInt(test);
+
+        assertEquals(result[0], actual[0]);
+        assertEquals(result[1], actual[1]);
+        assertEquals(result[2], actual[2]);
+        assertEquals(result[3], actual[3]);
+
+
+
     }
 }
